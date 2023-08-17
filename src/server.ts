c1e1317ea1bcd165e3cfc3455043f6295f4a4b51
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-08-17 15:49:21 -0300

    adicionando mais rotas

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@
 */
 import express from "express"
 import cors from "cors"
+import { foods, tags } from "./data"
 
 //instancia um novo servidor do express
 const app = express();
@@ -22,6 +23,38 @@ app.get("/api/foods", (req, res)=>{
     res.send(foods)
 })
 
+//get food by name
+app.get("/api/foods/search/:searchTerm", (req, res)=>{
+    //pega o valor passado na requisicao dos parametros /search/hamburguer
+    const searchTerm = req.params.searchTerm; 
+    //verifica se tem algum food com aquele nome
+    const filtred_foods = foods.filter(food=> food.name.toLowerCase()
+    .includes(searchTerm.toLowerCase()))
+
+    res.send(filtred_foods)
+})
+
+//get food by id
+app.get("/api/foods/:foodId", (req, res)=>{
+    const foodId = req.params.foodId;
+    const food = foods.find(food=> food.id == foodId)
+
+    res.send(food)
+})
+
+//get all tags
+app.get("/api/foods/tags", (req, res)=>{
+    res.send(tags)
+})
+
+//get foods by tag
+app.get("/api/foods/tags/:tagName", (req, res)=>{
+    const tagName = req.params.tagName; 
+    //filtra por todas as comidas que possui a tag com tagName passado
+    const tags_foods = foods.filter(food=> food.tags?.includes(tagName))
+
+    res.send(tags_foods)
+})
 
 //adicionando a porta ao servidor para escutar as requisicoes
 const port = 5000
